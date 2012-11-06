5adf915a5f30aff75e1e8e99e4c3eae7827eb68b
james026yeah <james026yeah@gmail.com>
2012-11-06 21:17:02 +0800

    add the mp3info

---
+++ src/com/example/musicplayer/Mp3Info.java
@@ -0,0 +1,12 @@
+package com.example.musicplayer;
+
+import java.io.Serializable;
+
+public class Mp3Info implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3508507506053617641L;
+
+}
--- src/com/example/musicplayer/MusicPlayService.java
+++ src/com/example/musicplayer/MusicPlayService.java
@@ -10,5 +10,17 @@ public class MusicPlayService extends Service {
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+	}
 	
 }
